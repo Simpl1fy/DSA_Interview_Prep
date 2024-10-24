65b3d7c7d0d5e2d5a7a1ff76053a8953657fe8f8
Simpl1fy <gourabdas07012004@gmail.com>
2024-10-24 12:20:02 +0530

    completed recursive sorting techniques -> Bubble, Insertion

--- sorting.cpp
+++ sorting.cpp
@@ -36,18 +36,57 @@ void insertion_sort(int arr[], int n) {
     }
 }
 
+void recursive_bubble_sort(vector<int> &arr, int n) {
+    if (n == 1) {
+        return;
+    }
+    for(int j=0; j<=n-1; j++) {
+        if (arr[j] > arr[j+1]) {
+            swap(arr[j], arr[j+1]);
+        }
+    }
+    recursive_bubble_sort(arr, n-1);
+}
+
+void recursive_insertion_sort(vector<int> &arr, int i, int n) {
+    if(i == n) {
+        return;
+    }
+    int j=i;
+    while (j > 0 && arr[j-1]>arr[j]) {
+        swap(arr[j-1], arr[j]);
+        j--;
+    }
+    recursive_insertion_sort(arr, i+1, n);
+}
+
 int main() {
+    // int n;
+    // cin >> n;
+    // int arr[n];
+    // for(int i=0; i<n; i++) {
+    //     cin >> arr[i];
+    // }   
+    // // selection_sort(arr, n);
+    // // bubble_sort(arr, n);
+    // insertion_sort(arr, n);
+    // for(int i=0; i<n; i++) {
+    //     cout << arr[i] << " ";
+    // }
+
+
+    vector<int> arr;
     int n;
     cin >> n;
-    int arr[n];
     for(int i=0; i<n; i++) {
-        cin >> arr[i];
-    }   
-    // selection_sort(arr, n);
-    // bubble_sort(arr, n);
-    insertion_sort(arr, n);
-    for(int i=0; i<n; i++) {
-        cout << arr[i] << " ";
+        int number;
+        cin >> number;
+        arr.push_back(number);
+    }
+    // recursive_bubble_sort(arr, n-1);
+    recursive_insertion_sort(arr, 1, n);
+    for(auto it: arr) {
+        cout << it << " ";
     }
     return 0;
 }
